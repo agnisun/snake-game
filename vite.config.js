792a7479266a8dce111f1ed9be749896e13e7d15
Daniil Parcvaniya <parcvaniya2021@gmail.com>
2024-07-14 15:28:02 +0300

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 
 export default defineConfig({
-    resolve: {
-        alias: [
-          { find: '@game', replacement: path.resolve(import.meta.dirname, 'src/game') },
-          { find: '@utils', replacement: path.resolve(import.meta.dirname, 'src/utils') },
-          { find: '@screens', replacement: path.resolve(import.meta.dirname, 'src/screens') },
-        ],
-    },
+  base: '/snake-game/',
+  resolve: {
+      alias: [
+        { find: '@game', replacement: path.resolve(import.meta.dirname, 'src/game') },
+        { find: '@utils', replacement: path.resolve(import.meta.dirname, 'src/utils') },
+        { find: '@screens', replacement: path.resolve(import.meta.dirname, 'src/screens') },
+      ],
+  },
 })
